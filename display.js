2467750f6d2478d86b1e12468cd998e4abe768b7
Santo83 <a_alonso_espadas@hotmail.com>
2018-12-12 11:53:16 +0100

    Add files via upload

---
+++ display.js
@@ -0,0 +1,59 @@
+ const SCALE = 20; 
+  
+  function createElement (type, className) {
+    let element = document.createElement(type);
+    if (className) element.className = className;
+    return element
+}
+
+function DOMDisplay(parent, level){
+  this.wrap = parent.appendChild(createElement('div', 'game'));
+  this.level = level;
+
+   this.wrap.appendChild(this.drawBackground());
+  // this.wrap.appendChild(this.drawActors());
+    this.actorsLayers = null;
+
+
+}
+
+DOMDisplay.prototype.drawBackground = function(){
+
+   let table = createElement('table', 'background');
+    table.style.width = this.level.width * SCALE + 'px';
+
+    this.level.grid.forEach(row => {
+        let rowElement = createElement('tr');
+        rowElement.style.height = SCALE + 'px';
+        table.appendChild(rowElement );
+        row.forEach(type => rowElement.appendChild(createElement('td', type)));
+    });
+
+    return table;
+
+}
+
+DOMDisplay.prototype.drawFrame = function(){
+    if(this.actorsLayers) this.wrap.removeChild(this.actorsLayers);
+    this.actorsLayers = this.wrap.appendChild(this.drawActors());
+    this.wrap.className = 'game' + (this.level.status || '')
+}
+
+DOMDisplay.prototype.drawActors = function () {
+    let actorsWrap = createElement('div');
+    this.level.actors.map(actor => {
+        let actorElement = createElement('div', `actor ${actor.type}`);
+        let rect = actorsWrap.appendChild(actorElement);
+        rect.style.width = actor.size.x * SCALE + 'px';
+        rect.style.height = actor.size.y * SCALE + 'px';
+        rect.style.left = actor.position.x * SCALE + 'px'; 
+        rect.style.top = actor.position.y * SCALE + 'px';
+    });
+
+    return actorsWrap;
+}
+
+DOMDisplay.prototype.clear = function(){
+    this.wrap.parentNode.removeChild(this.wrap);
+
+}
---
+++ level.js
@@ -0,0 +1,71 @@
+const ACTORS = {
+    'o': Coin,
+    '@': Player,
+    '=': Lava,
+    '|': Lava,
+    'v': Lava,
+
+};
+
+const MAX_STEP = 0.05;
+
+function Level(plan){
+    //validateLevel(plan)
+    this.width = plan[0].length;
+    this.height = plan.length;
+    this.status = null;
+    this.finishDelay = null;
+
+    this.grid = [];
+    this.actors = [];
+
+    for(let y = 0;  y < this.height; y++){
+        let line = plan[y];
+        //console.log(line)
+        let gridLine = []
+        for(let x = 0; x < this.width; x++ ){
+            let character = line[x];
+            //console.log(character);
+            let characterType = null;
+
+            let Actor = ACTORS[character]
+            console.log(Actor)
+            if(Actor) this.actors.push(new Actor(new Vector(x, y), character));
+            if (character === "x") characterType = "wall";
+            else if (character === "!") characterType = "lava"
+            
+            gridLine.push(characterType)
+
+        }
+        this.grid.push(gridLine)
+
+
+    }
+
+  this.actor= this.actors.filter(actor => actor.type == 'player')[0];
+     
+     console.log(this.actor);
+}
+
+Level.prototype.isFinished = function(){
+    return (this.status != null && this.finishDelay < 0 )
+}
+
+Level.prototype.animate = function (step, keys) {
+    if(this.status != null) this.finishDelay -= step;
+
+    while(step > 0){
+        let thisStep = Math.min(step, MAX_STEP);
+        this.actors.forEach(actor => actor.act(thisStep, this, keys));
+        step -= thisStep;
+    }
+
+
+}
+
+function validateLevel(level){
+    console.log(level)
+    for(let i = 0; 1 < level.length; i++){
+       console.log(level[i]) 
+    }
+}
